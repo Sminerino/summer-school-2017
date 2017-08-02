feb1277c650ba1ca1d7ec4f7e336088c138307a2
Nikolay Fedoseev <fedoseevn@gmail.com>
2017-08-02 09:18:24 +0500

    fixes

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
 import './promise-polyfill';
 import {app} from './app';
 
-import 'styles/style.scss';
-
 // Enable progressive web app support (with offline-plugin)
 if (process.env.NODE_ENV === 'production') {
     require('./pwa');
--- client/views/App.vue
+++ client/views/App.vue
@@ -2,29 +2,37 @@
     <div class="page">
         <div class="container">
             <h1 class="page__title">Welcome to Xsolla Summer School 2017!</h1>
+            <img src="https://cdn.xsolla.net/xsolla-live/files/pages/press-kit/Main/Xsolla-main.svg">
         </div>
     </div>
 </template>
 
 <style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
     html, body {
-        padding-top: 20px;
         height: 100%;
     }
 
     body {
         background-color: #fff;
-        background-image: url('https://cdn.xsolla.net/xsolla-live/files/pages/press-kit/Main/Xsolla-main.svg');
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: 50%;
         font-family: 'Roboto', sans-serif;
         color: #1d1d1d;
     }
 
+    img {
+        width: 500px;
+        height: 500px;
+    }
+
     .container {
+        padding: 50px;
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        align-items: center;
     }
 
     .page__title {
