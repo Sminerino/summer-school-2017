62d55712789b229c6910182ffe003b99f99a4888
nicoffee <fedoseevn@gmail.com>
2017-08-04 13:12:36 +0500

    add api call

---
+++ client/CityCard.js
@@ -0,0 +1,31 @@
+import styles from 'style.css';
+
+class City {
+    constructor(data) {
+        this.imgUrl = data.data.current.condition.icon;
+        this.city = data.data.location.name;
+        this.temp = data.data.current.temp_c;
+    }
+
+    render() {
+        const template = `
+        <div class=${styles.city-card}>
+           <img class="city-card__img" src="${this.imgUrl}">
+           <span class="city-card__condition">
+             Overcast
+           </span>
+           <span class="city-card__temperature">
+             ${this.temp}
+           </span>
+           <span class="city-card__city">
+             ${this.city}
+           </span>
+         </div>`;
+
+        document.
+            getElementById('weather_container')
+            .innerHTML += template;
+    }
+}
+
+export default City;
--- client/index.js
+++ client/index.js
@@ -1,2 +1,17 @@
+import { apiKey } from './../config.json';
+import axios from 'axios';
+import CityCard from './CityCard';
 
+const cityName = 'Perm';
 
+console.log('CityCard', CityCard);
+
+document.getElementById('add_weather').onclick = () =>
+{
+    axios
+        .get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${cityName}`)
+        .then((res) => {
+            const City = new CityCard(res);
+            City.render();
+        });
+};
